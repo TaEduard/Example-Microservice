55b9b31393288d6d53eb439892ee40d4fa712541
Eduard Tamsa <eduard.ioan.tamsa@gmail.com>
2024-03-10 02:26:11 +0200

    Cleanup unused providers

--- Infra/versions.tf
+++ Infra/versions.tf
@@ -4,10 +4,6 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 3.0"
     }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "~> 2.0"
-    }
     random = {
       source  = "hashicorp/random"
       version = "~> 3.0"
@@ -18,12 +14,4 @@ terraform {
 provider "azurerm" {
   features {}
 }
-
-provider "kubernetes" {
-  host                   = azurerm_kubernetes_cluster.aks_cluster.kube_config.0.host
-  client_certificate     = base64decode(azurerm_kubernetes_cluster.aks_cluster.kube_config.0.client_certificate)
-  client_key             = base64decode(azurerm_kubernetes_cluster.aks_cluster.kube_config.0.client_key)
-  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks_cluster.kube_config.0.cluster_ca_certificate)
-}
-
 provider "random" {}
